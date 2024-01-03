ec9f51eb23348efa7f8599d537efe1b98a1fb15b
thouay-baccam <thouay.baccam@laplateforme.io>
2024-01-03 09:38:00 +0100

    Ajout job 02

---
+++ jour02/job02.py
@@ -0,0 +1,44 @@
+class Livre:
+    def __init__(self, titre, auteur, pages):
+        self.__titre = titre
+        self.__auteur = auteur
+        if isinstance(pages, int) and pages > 0:
+            self.__pages = pages
+        else:
+            print("Erreur : Le nombre de pages doit être plus que 0.")
+            self.__pages = None
+
+    def get_titre(self):
+        return self.__titre
+
+    def get_auteur(self):
+        return self.__auteur
+
+    def get_pages(self):
+        return self.__pages
+
+    def set_titre(self, nouveau_titre):
+        self.__titre = nouveau_titre
+
+    def set_auteur(self, nouveau_auteur):
+        self.__auteur = nouveau_auteur
+
+    def set_pages(self, nouvelle_pages):
+        if isinstance(nouvelle_pages, int) and nouvelle_pages > 0:
+            self.__pages = nouvelle_pages
+        else:
+            print("Erreur : Le nombre de pages doit être plus que 0.")
+
+mon_livre = Livre("Watchmen", "Alan Moore", 416)
+
+print(f"Titre : {mon_livre.get_titre()}")
+print(f"Auteur : {mon_livre.get_auteur()}")
+print(f"Pages : {mon_livre.get_pages()}")
+
+mon_livre.set_titre("Le Portrait de Dorian Gray")
+mon_livre.set_auteur("Oscar Wilde")
+mon_livre.set_pages(258)
+
+print(f"Titre après modif : {mon_livre.get_titre()}")
+print(f"Auteur après modif : {mon_livre.get_auteur()}")
+print(f"Pages après modif : {mon_livre.get_pages()}")
