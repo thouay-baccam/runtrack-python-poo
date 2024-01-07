8720a00dc313f24d3404825cccecaa78ac1227c2
thouay-baccam <thouay.baccam@laplateforme.io>
2024-01-07 15:01:43 +0100

    Fini job 06

--- jour04/job06.py
+++ jour04/job06.py
@@ -1,4 +1,5 @@
 class Vehicule:
+
     def __init__(self, marque, modele, annee, prix):
         self.marque = marque
         self.modele = modele
@@ -11,16 +12,24 @@ class Vehicule:
         print(f"Année = {self.annee}")
         print(f"Prix = {self.prix}")
 
+    def demarrer(self):
+        print("Attention, je roule")
+
 class Voiture(Vehicule):
+
     def __init__(self, marque, modele, annee, prix, portes=4):
         super().__init__(marque, modele, annee, prix)
         self.portes = portes
 
     def informationsVehicule(self):
         super().informationsVehicule()
-        print(f"Nombre de portes = {self.portes}\n")
+        print(f"Nombre de portes = {self.portes}")
+    
+    def demarrer(self):
+        print(f"La {self.marque} {self.modele} démarre et elle fonce!\n")
 
 class Moto(Vehicule):
+
     def __init__(self, marque, modele, annee, prix, portes=2):
         super().__init__(marque, modele, annee, prix)
         self.portes = portes
@@ -29,8 +38,14 @@ class Moto(Vehicule):
         super().informationsVehicule()
         print(f"Nombre de portes = {self.portes}")
 
+    def demarrer(self):
+        print(f"La {self.marque} {self.modele} démarre et atteint 100km/h rapidement !")
+
+
 mercedes = Voiture(marque="Mercedes", modele="Classe A", annee=2020, prix=18500)
 mercedes.informationsVehicule()
+mercedes.demarrer()
 
 yamaha = Moto(marque="Yamaha", modele="1200 Vmax", annee=1987, prix=4500)
 yamaha.informationsVehicule()
+yamaha.demarrer()
