2f9fcadcf11d83593ce0e0abdc9cd7ddbd682e5f
thouay-baccam <thouay.baccam@laplateforme.io>
2024-01-07 14:56:54 +0100

    Ajout job 06 moto

--- jour04/job06.py
+++ jour04/job06.py
@@ -18,7 +18,19 @@ class Voiture(Vehicule):
 
     def informationsVehicule(self):
         super().informationsVehicule()
+        print(f"Nombre de portes = {self.portes}\n")
+
+class Moto(Vehicule):
+    def __init__(self, marque, modele, annee, prix, portes=2):
+        super().__init__(marque, modele, annee, prix)
+        self.portes = portes
+
+    def informationsVehicule(self):
+        super().informationsVehicule()
         print(f"Nombre de portes = {self.portes}")
 
 mercedes = Voiture(marque="Mercedes", modele="Classe A", annee=2020, prix=18500)
 mercedes.informationsVehicule()
+
+yamaha = Moto(marque="Yamaha", modele="1200 Vmax", annee=1987, prix=4500)
+yamaha.informationsVehicule()
